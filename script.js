e2d59a2676a6b6a53e41e6f54dddd02f68314501
Jonah <jonahkesslercohen@gmail.com>
2021-12-27 02:47:12 +0000

    added google analytics

--- script.js
+++ script.js
@@ -1,62 +1,16 @@
-/* status:
-var barbaraStatusText = document.getElementById('barbara-up');
-var p = new Ping({timeout: '1000'});
-var bp = new Ping({
-	favicon: '/static/favicon.ico',
-	timeout: '1000'
+let statusElement = document.getElementById("status");
+
+// Ping https://Barbara-Release.turnip123.repl.co, and set the element with the id of "status" to the value of the response, and if there's an error, set the element with the id of "status" to "Barbara is down."
+fetch("https://Barbara-Release.turnip123.repl.co/", {
+  method: "GET",
+  mode: "no-cors",
+}).then(response => {
+  if (response) {
+    response.text().then(text => {
+      statusElement.innerHTML = text;
+      console.log(text);
+    });
+  } else {
+    statusElement.innerHTML = "Barbara is down.";
+  }
 });
-var responses = 0;
-var response_status = [0, 0, 0]
-const barbara_down = [1, -1, 1]
-const barbara_up = [1, 1, 1]
-const wifi_down = [-1, -1, -1]
-p.ping("https://www.google.com", (err, data) => {
-	if (err) {
-    console.log("Error loading Google");
-		data = -1
-  }
-  response_status[0] = data;
-	updateStatus()
-	responses++
-})
-bp.ping("https://Barbara-backend.turnip123.repl.co", (err, data) => {
-	if (err) {
-    console.log("Error loading Barbara-backend");
-		data = -1
-  }
-  response_status[1] = data;
-	updateStatus()
-	responses++
-})
-p.ping("https://github.com", (err, data) => {
-	if (err) {
-    console.log("Error loading GitHub");
-		data = -1
-  }
-  response_status[2] = data;
-	updateStatus()
-	responses++
-})
-function updateStatus() {
-	if(responses == 2) {
-		if (response_status[0] >= 0) {
-			response_status[0] = 1;
-		}
-		if (response_status[1] >= 0) {
-			response_status[1] = 1;
-		}
-		if (response_status[2] >= 0) {
-			response_status[2] = 1;
-		}
-		console.log(response_status);
-		if(response_status == wifi_down){
-			barbaraStatusText.innerText = "Not Connected to Wifi!";
-		} else if(response_status == barbara_down) {
-			barbaraStatusText.innerText = "Barbara is OFFLINE";
-		} else if(response_status == barbara_up) {
-			barbaraStatusText.innerText = "Barbara is ONLINE";
-		} else {
-			barbaraStatusText.innerText = `An error has occured fetching bot status, send the following code to us at bugs@jcwyt.com for troubleshooting: {${response_status[0]}, ${response_status[1]}, ${response_status[2]}}`;
-		}
-	}
-}
