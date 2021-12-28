ec3c4c3072cbb70d7787c5bada97b824f61374fd
Silas Hunter-Hill <mauveshishito14@gmail.com>
2021-12-28 21:30:38 +0000

    status work

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let statusElement = document.getElementById("status");
+window.addEventListener('load', function() {
+  let statusElement = document.getElementById("status");
 
-// Ping https://Barbara-Release.turnip123.repl.co, and set the element with the id of "status" to the value of the response, and if there's an error, set the element with the id of "status" to "Barbara is down."
-fetch("https://Barbara-Release.turnip123.repl.co/", {
-  method: "GET",
-  mode: "no-cors",
-}).then(response => {
-  if (response) {
-    response.text().then(text => {
-      statusElement.innerHTML = text;
-      console.log(text);
-    });
-  } else {
-    statusElement.innerHTML = "Barbara is down.";
-  }
+  fetch("https://status.barbara.jcwyt.com", {
+    method: "GET",
+  }).then(response => {
+    if (response) {
+      response.text().then(text => {
+        statusElement.innerHTML = `Barbara is online and in ${text} servers.`;
+      });
+    } else {
+      statusElement.innerHTML = "Barbara is down.";
+    }
+  });
 });
