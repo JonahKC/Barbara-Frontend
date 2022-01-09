08547defffed9dc3e4f8d29d051a24f18718706c
Silas Hunter-Hill <mauveshishito14@gmail.com>
2022-01-09 23:19:34 +0000

    Make barbara down message work with XMLHTTPRequests

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 window.addEventListener('load', function() {
   let statusElement = document.getElementById("status");
+  let request = new XMLHttpRequest();
+  request.open('GET', "https://status.barbara.jcwyt.com");
+  request.onerror = function(error) {
+    
+    // This is a bit confusing. A CORS error happens when the server is down
+    // And the browser tries to hide the CORS error, so I can't check for it using normal error handling
+    // So what I'm doing here is checking if error.response is undefined, which generally means the browser
+    // has hidden the CORS error
+    if (typeof error.response === 'undefined') {
+      
+      // And after we figure out that the server is down, we say that
+      statusElement.innerHTML = "Barbara is down.";
 
-  fetch("https://status.barbara.jcwyt.com", {
-    method: "GET",
-  }).then(response => {
-    if (response) {
-      response.text().then(text => {
-        statusElement.innerHTML = `Barbara online and on ${text} servers.`;
-      });
+      // And then stop the function
+      return;
+    }
+  }
+
+  request.onload = function() {
+    if (request.responseText) {
+      statusElement.innerHTML = `Barbara online and on ${request.responseText} servers.`;
     } else {
       statusElement.innerHTML = "Barbara is down.";
     }
-  });
+  };
+
+  request.send();
 });
